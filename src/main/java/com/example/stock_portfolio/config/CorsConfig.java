85fcdf25bc7ee1040c4cbc69c0c1929c9abe8521
Udayan <nathuk50@gmail.com>
2025-01-05 14:01:23 +0530

    cors changes

--- src/main/java/com/example/stock_portfolio/config/CorsConfig.java
+++ src/main/java/com/example/stock_portfolio/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
