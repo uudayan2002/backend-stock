1af654a18344c274cea6e1826f657bbfacaf19c8
Udayan <nathuk50@gmail.com>
2025-01-06 20:34:00 +0530

    some changes

--- src/main/java/com/example/stock_portfolio/config/CorsConfig.java
+++ src/main/java/com/example/stock_portfolio/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOriginPatterns("https://portfoliostock.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
